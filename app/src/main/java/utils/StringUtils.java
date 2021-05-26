5f60a31270c1d29acdade071f7038c499d8322b9
Roman <roma.bruhanchik@gmail.com>
2021-05-26 20:30:11 +0300

    StringUtils class and test

---
+++ app/src/main/java/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class StringUtils {
+
+    private StringUtils() {}
+
+    public static boolean isPositiveNumber(String str) {
+        return NumberUtils.isCreatable(str) && NumberUtils.createInteger(str) > 0;
+    }
+}
